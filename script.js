e59d97d086a8fcc79cab70c3431678d9d9b92c64
rishikeshydv <rishikeshadh4@gmail.com>
2023-02-03 01:34:28 -0500

    final changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("first").innerHTML = "Lorem Epsum Changed";
+    document.getElementById("first").style.fontSize = "40px";
+    document.getElementById("first").style.color = "red";
+}
+  
+function myFunction1() {
+    document.getElementById('img1').src = "cross.jpeg";
+}
