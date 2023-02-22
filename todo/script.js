e0b01b15650846493822c2a739d60f462d25e921
rishikeshydv <rishikeshadh4@gmail.com>
2023-02-22 02:17:09 -0500

    final commit

---
+++ todo/script.js
@@ -0,0 +1,46 @@
+let input = document.querySelector('.todo-input');
+let addBtn = document.querySelector('.todo-submit');
+let tasks = document.querySelector('.tasks');
+
+//enable & disable add button
+input.addEventListener('keyup', () => {
+    if (input.value.trim() != 0){
+        addBtn.classList.add('active')
+    }
+    else {
+        addBtn.classList.remove('active')
+    }
+})
+
+//adding new tasks to list
+addBtn.addEventListener('click', () => {
+    if (input.value.trim() != 0) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('items');
+        newItem.innerHTML = `
+        <p> ${input.value}</p>
+        <div class="item-btn">
+      <i class="fa-regular fa-pen-to-square"></i>
+      <i class="fa-regular fa-rectangle-xmark"></i>
+    </div>
+    `;
+    tasks.appendChild(newItem);
+    input.value = '';
+    } else {
+        alert('Please enter a task')
+    }
+})
+
+//delete from list
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-rectangle-xmark')){
+        e.target.parentElement.parentElement.remove();
+    }
+})
+
+//mark as complete
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-pen-to-square')){
+        e.target.parentElement.parentElement.classList.toggle('completed');
+    }
+})
